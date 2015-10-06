57d771db5830b4b91a463c3940e7e72739872eec
mohammed519 <mkpasha519@gmail.com>
2015-10-05 22:15:25 -0400

    this is modified fro screenshot

--- Hello.java
+++ Hello.java
@@ -5,5 +5,6 @@ public static void maiin (String args[])
 System.out.println(" Hello, This is mohammed");
 System.out.println("This is edited version");
 System.out.println("This is second time edited");
+System.out.println("This is for screenshot update");
 }
 }
